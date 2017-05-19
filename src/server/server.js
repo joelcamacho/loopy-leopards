ba5fb7c9494b54e7107ed447f769958f1a31be31
huawillian <huawillian@gmail.com>
2017-05-19 10:15:50 -0400

    serving client files on port 3000

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,20 @@
-const app = require('express')();
+// dependencies
+const express = require('express');
+const morgan  = require('morgan');
+
+const app = express();
 const routes = require('./routes');
+
+// add morgan middleware logger
+app.use(morgan('short'))
+
+// serve client app
+app.use(express.static(__dirname + '/../client'));
 
 //  Connect routes
 app.use('/', routes);
 
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('App listening on port', (process.env.PORT || 3000));
 });

14a2dad390cbcd001a8b077098af6952469aad0e
Guangyu Xu <davidxu198556@gmail.com>
2017-05-20 00:17:01 -0400

    eventbrite Api setup

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,11 +1,13 @@
 const routes = require('express').Router();
-const yelpRoutes = require('./yelp.routes.js')
+const yelpRoutes = require('./yelp.routes.js');
+const eventbriteRoutes = require('./eventbrite.routes.js')
 
 routes.get('/', (req, res) => {
   res.status(200).json({ message: 'Connected!' });
 });
 
 routes.use(yelpRoutes);
+routes.use(eventbriteRoutes);
 
 
 module.exports = routes;

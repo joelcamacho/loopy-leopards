1b6ac7859c0a04ebb18328008b3c4a981df45058
huawillian <huawillian@gmail.com>
2017-05-23 11:10:39 -0400

    Got some list

--- src/client/app/components/group.component.jsx
+++ src/client/app/components/group.component.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import Avatar from 'material-ui/Avatar';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
+import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
 
 export default class GroupPageComponent extends React.Component {
   constructor(props) {
@@ -7,6 +12,52 @@ export default class GroupPageComponent extends React.Component {
 
   render() {
     console.log(this.props.group);
-    return (<div> GROUP COMPONENT </div>);
+    return (
+      <div>
+        <div>
+          <h3> GROUP </h3>
+          <List>
+            <Subheader> Loopy Leopards   </Subheader>
+            <ListItem
+              primaryText="Brendan Lim"
+              leftAvatar={<Avatar src="images/ok-128.jpg" />}
+              rightIcon={<CommunicationChatBubble />}
+            />
+            <ListItem
+              primaryText="Eric Hoffman"
+              leftAvatar={<Avatar src="images/kolage-128.jpg" />}
+              rightIcon={<CommunicationChatBubble />}
+            />
+            <ListItem
+              primaryText="Grace Ng"
+              leftAvatar={<Avatar src="images/uxceo-128.jpg" />}
+              rightIcon={<CommunicationChatBubble />}
+            />
+            <ListItem
+              primaryText="Kerem Suer"
+              leftAvatar={<Avatar src="images/kerem-128.jpg" />}
+              rightIcon={<CommunicationChatBubble />}
+            />
+            <ListItem
+              primaryText="Raquel Parrado"
+              leftAvatar={<Avatar src="images/raquelromanp-128.jpg" />}
+              rightIcon={<CommunicationChatBubble />}
+            />
+          </List>
+          <Divider />
+          <List>
+            <Subheader>Previous chats</Subheader>
+            <ListItem
+              primaryText="Chelsea Otakan"
+              leftAvatar={<Avatar src="images/chexee-128.jpg" />}
+            />
+            <ListItem
+              primaryText="James Anderson"
+              leftAvatar={<Avatar src="images/jsa-128.jpg" />}
+            />
+          </List>
+        </div>
+      </div>
+    );
   }
 }

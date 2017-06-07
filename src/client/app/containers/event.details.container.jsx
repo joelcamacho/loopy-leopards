abc926831761ff181762c410048803a1402ed1ea
Joel Camacho <jcamacho127@gmail.com>
2017-06-07 19:11:47 -0400

    Progress on event invitee event detail view, may need additional logic

--- src/client/app/containers/event.details.container.jsx
+++ src/client/app/containers/event.details.container.jsx
@@ -3,13 +3,20 @@ import EventDetailsPageComponent from '../components/event.details.component.jsx
 import action from '../actions/events.action.jsx';
 
 export default connect(
-  function mapStateToProps(state) {
-    return { 
-      profile: state.profile.toJS(),
-      event: state.event,
-    };
-  },
-  function mapDispatchToProps(dispatch) {
-    return {};
-  }
+	function mapStateToProps(state) {
+		return { 
+			profile: state.profile.toJS(),
+			event: state.event,
+		};
+	},
+	function mapDispatchToProps(dispatch) {
+		return {
+			getEventDetails: function(event) {
+				return dispatch(EventAction.eventDetails(event));
+			},
+			updateEvent: function(group) {
+				return dispatch(EventAction.updateEvent(event));
+			}
+		};
+	}
 )(EventDetailsPageComponent);
